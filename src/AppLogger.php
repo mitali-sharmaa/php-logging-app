fe46b8f5e6d577ffc0ac39b45c00b8671840502a
Abdun Nayeem Khan <nayeem0073297@gmail.com>
2024-10-28 20:59:14 -0400

    done

---
+++ src/AppLogger.php
@@ -0,0 +1,23 @@
+<?php
+namespace App;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+class AppLogger {
+    private $logger;
+
+    public function __construct() {
+        $this->logger = new Logger('appLogger');
+        $this->logger->pushHandler(new StreamHandler(__DIR__ . '/../logs/app.log', Logger::DEBUG));
+        $this->logger->pushHandler(new StreamHandler(__DIR__ . '/../logs/error.log', Logger::ERROR));
+    }
+
+    public function logInfo($message) {
+        $this->logger->info($message);
+    }
+
+    public function logError($message) {
+        $this->logger->error($message);
+    }
+}
